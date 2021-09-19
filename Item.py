e5022654be57d110a56aabfe0b79c04531a82418
SVoldemar <skilletwup@gmail.com>
2021-09-19 14:14:47 +0300

    Working on Buy/Sell Items: -added class BuyItem(blank) -added class SellItem(blank)

--- Item.py
+++ Item.py
@@ -3,10 +3,9 @@ import json
 from selenium import webdriver
 
 
-class BItems:
+class BuyItems:
     def __init__(self):
         self.items = []
-        self.b_pos = 0
         options = webdriver.ChromeOptions()
         self.brow = webdriver.Chrome(chrome_options=options)
         self.brow.get("https://steamcommunity.com/")
@@ -24,31 +23,46 @@ class BItems:
                            'mp': marketplace})
 
     def buy_item(self):
-        if self.items[self.b_pos]['mp'] == 'scrap':
+        if self.items[0]['mp'] == 'scrap':
             # add here
-            self.b_pos += 1
-        elif self.items[self.b_pos]['mp'] == 'backpack':
+            del self.items[0]
+        elif self.items[0]['mp'] == 'backpack':
             # add here
-            self.b_pos += 1
+            del self.items[0]
         else:
-            print(self.items[self.b_pos], " Wrong marketplace")
-            del self.items[self.b_pos]
+            print(self.items[0], " Wrong marketplace")
+            del self.items[0]
+
+
+class SellItems:
+    def __init__(self):
+        self.items = []
+        options = webdriver.ChromeOptions()
+        self.brow = webdriver.Chrome(chrome_options=options)
+        self.brow.get("https://steamcommunity.com/")
+        file = open("cookies.txt", 'r')
+        cookies = json.load(file)
+        for cookie in cookies:
+            self.brow.add_cookie(cookie)
+        file.close()
+        self.brow.refresh()
+
+    def add_item(self, name, price, count, marketplace):
+        self.items.append({'name': name.replace(" ", "%20"),
+                           'price': price,
+                           'count': count,
+                           'mp': marketplace})
 
     def sell_item(self):
-        if self.b_pos >= 0:
-            if self.items[0]['mp'] == 'scrap':
-                # add here
-                del self.items[0]
-                self.b_pos -= 1
-            elif self.items[0]['mp'] == 'backpack':
-                # add here
-                del self.items[0]
-                self.b_pos -= 1
-            else:
-                print(self.items[0], " Wrong marketplace")
-                del self.items[0]
+        if self.items[0]['mp'] == 'scrap':
+            # add here
+            del self.items[0]
+        elif self.items[0]['mp'] == 'backpack':
+            # add here
+            del self.items[0]
         else:
-            print('Empty buy_list')
+            print(self.items[0], " Wrong marketplace")
+            del self.items[0]
 
 if __name__ == '__main__':
-    item_list = BItems()
+    item_list = SellItems()
