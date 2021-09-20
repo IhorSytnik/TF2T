f67a826842802aa6a7bf29cca52ede16c971ba55
IgorSytnik <kintys.sytnik@gmail.com>
2021-09-20 19:45:39 +0300

    Finishing on Scrap parsing -put main part into the parse_scrap() function -added documentation

--- parseScrap.py
+++ parseScrap.py
@@ -1,7 +1,6 @@
 import json
 from enum import Enum
 from selenium import webdriver
-import helping_function
 from bs4 import BeautifulSoup as Bs
 
 
@@ -11,6 +10,8 @@ class Quality(Enum):
 
     First value - BackPack.tf, Regular value
     Second value - Scrap.tf
+
+    Note: Is not used in the program right now.
     """
     Normal = "Normal"
     Unique = "Unique", "quality6"
@@ -26,30 +27,87 @@ class Quality(Enum):
     Valve = "Valve"
 
 
-# Creating browser object
-options = webdriver.ChromeOptions()
-browser = webdriver.Chrome(options=options)
+def _save_cookie(driver, path):
+    """
+    Saves a cookie file.
 
-# Waiting for user to log in
-browser.get("https://scrap.tf/login")
-input()
+    :param driver: a web driver object that you want to get cookies from
+    :param path: path to a cookie file
 
-browser.get("https://scrap.tf/buy/hats")
-try:
-    as_info = browser.find_element_by_id('category-2')
-    products = as_info.find_elements_by_tag_name('div')
-except():
-    print("Can't find element with id 'category-2'")
-    exit(1)
-finally:
-    pass
+    Note: Is not used in the program right now.
+    """
+    with open(path, 'w') as file_handler:
+        json.dump(driver.get_cookies(), file_handler)
 
-html = browser.page_source
-html = Bs(html, 'html.parser')
-html = html.find('div', attrs={"id": "category-2"}) \
-    .find('div', attrs={"class": "items-container"})
-items_div = html.find_all('div')
-print(items_div)
-# bo_price = bo_prices[0].text.strip()
 
-browser.close()
+def _load_cookies(driver, path):
+    """
+    Loads cookies into the web driver.
+
+    :param driver: a web driver object that you want to load cookies into
+    :param path: path to a cookie file
+    """
+    with open(path, 'r') as cookies_file:
+        cookies = json.load(cookies_file)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+
+def parse_scrap():
+    """
+    Parses Scrap.tf and then returns dictionary of items
+
+    :return: items - item dictionary that looks like this:
+            item = {
+                'name': item name,
+                'price': price in scrap metal,
+                'painted': paint name if painted,
+                'available': quantity of this item available
+            }
+        Examples:
+            item_1 = {
+                'name': 'Mann of the Seven Sees',
+                'price': '1797',
+                'painted': 'The Value of Teamwork',
+                'available': '1'
+            }
+            item_2 = {
+                'name': 'Byte'd Beak',
+                'price': '627',
+                'painted': '',
+                'available': '3'
+            }
+    """
+    # Creating browser object
+    options = webdriver.ChromeOptions()
+    browser = webdriver.Chrome(options=options)
+
+    # Loading saved cookies
+    browser.get("https://scrap.tf")
+    _load_cookies(browser, "cookiesScrap")
+    browser.get("https://scrap.tf/buy/hats")
+
+    # Getting all item divs from web site
+    html = browser.page_source
+    html = Bs(html, 'html.parser')
+    html = html.find('div', attrs={"id": "category-2"}) \
+        .find('div', attrs={"class": "items-container"})
+    items_div = html.contents
+
+    # Compiling items into dictionary
+    items = []
+    for item in list(filter(lambda e: e != "\n", items_div)):
+        data_content = Bs(item['data-content'], 'html.parser')
+        brs = data_content.contents
+        painted = list(filter(lambda e: str(e).startswith('Painted'), brs))
+        name = Bs(item['data-title'], 'html.parser')
+        span = name.find_all()
+        items.append({
+            'name': str(name if len(span) == 0 else span[0].contents[0]),
+            'price': item['data-item-value'],
+            'painted': str('' if len(painted) == 0 else painted[0].removeprefix('Painted ')),
+            # 'paint_hex': '',
+            'available': item['data-num-available']
+        })
+    browser.close()
+    return items
